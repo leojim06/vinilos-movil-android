2c26124b5d9c9474e8a002c8f4e5112c6ee56358
Héctor Arias <h.arias@uniandes.edu.co>
2021-10-29 20:49:38 -0500

    Se agrega linea base de la aplicacion

---
+++ app/src/main/java/com/example/vinilosapp/MainActivity.kt
@@ -0,0 +1,17 @@
+package com.example.vinilosapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.content.Intent
+import android.view.View
+
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+
+
+
+}
