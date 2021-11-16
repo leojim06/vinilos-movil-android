94f453393491056827e57f4daea3379db9c29c71
Juan Feliciano <jp.feliciano10@uniandes.edu.co>
2021-11-15 22:59:36 -0500

    Avance implementación: Vista utilizando fragmentos para mostrar listado de coleccionistas. Clase con representación del modelo, Repositorio y Service Adapter.

--- app/src/main/java/com/example/vinilosapp/data/api/NetworkServiceAdapter.kt
+++ app/src/main/java/com/example/vinilosapp/data/api/NetworkServiceAdapter.kt
@@ -9,14 +9,13 @@ import com.android.volley.VolleyError
 import com.android.volley.toolbox.JsonObjectRequest
 import com.android.volley.toolbox.StringRequest
 import com.android.volley.toolbox.Volley
-import com.example.vinilosapp.data.model.Album
+import com.example.vinilosapp.data.model.Collector
 import org.json.JSONArray
-import org.json.JSONException
 import org.json.JSONObject
 
 class NetworkServiceAdapter constructor(context: Context) {
-    companion object{
-        const val BASE_URL= "https://back-vinyls-populated.herokuapp.com/"
+    companion object {
+        const val BASE_URL = "https://back-vinyls-populated.herokuapp.com/"
         var instance: NetworkServiceAdapter? = null
         fun getInstance(context: Context) =
             instance ?: synchronized(this) {
@@ -25,29 +24,78 @@ class NetworkServiceAdapter constructor(context: Context) {
                 }
             }
     }
+
     private val requestQueue: RequestQueue by lazy {
         // applicationContext keeps you from leaking the Activity or BroadcastReceiver if someone passes one in.
         Volley.newRequestQueue(context.applicationContext)
     }
 
-    val stringRequest = StringRequest(
-        Request.Method.GET,
-        BASE_URL+"albums",
-        Response.Listener<String> { response ->
-            Log.d("Response", response)
-            try {
-                val resp = JSONArray(response)
-                val list = mutableListOf<Album>()
-                for (i in 0 until resp.length()) {
-                    val item = resp.getJSONObject(i)
-                    list.add(i, Album(id = item.getInt("id"),name = item.getString("name"), cover = item.getString("cover"), recordLabel = item.getString("recordLabel"), releaseDate = item.getString("releaseDate"), genre = item.getString("genre"), description = item.getString("description")))
-                }
+    fun getCollectors(
+        onComplete: (resp: List<Collector>) -> Unit,
+        onError: (error: VolleyError) -> Unit
+    ) {
+        requestQueue.add(
+            getRequest("collectors",
+                Response.Listener<String> { response ->
+                    Log.d("tagb", response)
+                    val resp = JSONArray(response)
+                    val list = mutableListOf<Collector>()
+                    for (i in 0 until resp.length()) {
+                        val item = resp.getJSONObject(i)
+                        list.add(
+                            i,
+                            Collector(
+                                collectorId = item.getInt("id"),
+                                name = item.getString("name"),
+                                telephone = item.getString("telephone"),
+                                email = item.getString("email")
+                            )
+                        )
+                    }
+                    onComplete(list)
+                },
+                Response.ErrorListener {
+                    onError(it)
+                    Log.d("", it.message.toString())
+                })
+        )
+    }
 
-            } catch (e: JSONException) {
-                Log.d("Parsing Issue:", e.localizedMessage)
-                "Parsing Issue"
-            }
-        },  //...3
-        Response.ErrorListener { "Response fail" })  //...4
+    private fun getRequest(
+        path: String,
+        responseListener: Response.Listener<String>,
+        errorListener: Response.ErrorListener
+    ): StringRequest {
+        return StringRequest(Request.Method.GET, BASE_URL + path, responseListener, errorListener)
+    }
 
+    private fun postRequest(
+        path: String,
+        body: JSONObject,
+        responseListener: Response.Listener<JSONObject>,
+        errorListener: Response.ErrorListener
+    ): JsonObjectRequest {
+        return JsonObjectRequest(
+            Request.Method.POST,
+            BASE_URL + path,
+            body,
+            responseListener,
+            errorListener
+        )
+    }
+
+    private fun putRequest(
+        path: String,
+        body: JSONObject,
+        responseListener: Response.Listener<JSONObject>,
+        errorListener: Response.ErrorListener
+    ): JsonObjectRequest {
+        return JsonObjectRequest(
+            Request.Method.PUT,
+            BASE_URL + path,
+            body,
+            responseListener,
+            errorListener
+        )
+    }
 }
--- app/src/main/java/com/example/vinilosapp/ui/MainActivity.kt
+++ app/src/main/java/com/example/vinilosapp/ui/MainActivity.kt
@@ -1,7 +1,7 @@
 package com.example.vinilosapp.ui
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.setupWithNavController
@@ -9,13 +9,13 @@ import com.example.vinilosapp.R
 import com.example.vinilosapp.ui.factory.AlbumsFragmentFactory
 import kotlinx.android.synthetic.main.activity_main.*
 
-class  MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity() {
 
     private val navHostFragment by lazy {
         supportFragmentManager.findFragmentById(R.id.nav_holder) as NavHostFragment
     }
 
-    private val appBarConfiguration : AppBarConfiguration by lazy {
+    private val appBarConfiguration: AppBarConfiguration by lazy {
         AppBarConfiguration(navHostFragment.navController.graph)
     }
 
